1f76062bd71ee9f7e56f08f4ff2205b32d9d60cb
zzamytikk <zzamytikk@gmail.com>
2024-10-01 22:59:58 +0300

    update

--- zONas/1-0-0/ok.js
+++ zONas/1-0-0/ok.js
@@ -3,73 +3,114 @@
     © Copyright Плюшки для сайтов 2024
 */
 var zONas = {//Всплывающее окно `Настройки/Разное`
-  //zONas.$();//★ Всплывающее окно `Настройки/Разное` Запускаем!
-  $: () => { //Вешаем click
-    let O = zONas;
-
-    $('[class*="zONas-"]').find('>button:eq(0),>:eq(0) button').each((i, e) => {
-      if ($._data($(e)[0], 'events')?.click[0].namespace != 'zONas') { //Проверка ключа `Ключь не совпал! повесим обработчик`
-        $(e).on('click.zONas', e => {O.C($(e.currentTarget))});
-      } else {
-        console.debug('Была попытка повторного запуска скрипта zONas.$();');
-      }
-    });
+  //F:1,//Ожидается подгрузка содержания (zONas.$({F: () => {}});)
+  /*  //Запускает поиск всех class="zONas-.." (Кроме тех у которых установлен id="svoi"):
+      zONas.$();//★ Всплывающее окно `Настройки/Разное` Запускаем!
+       
+        || • Вариант 2:
+      zONas.$({//★ Запускаем индивидуально для подгруза
+        id: 'svoi', //Путь до <div id="svoi" class="zONas-
+        
+        F: (d, b, N) => { //Своя функция (Сработает при открытии окна)
+          //d = $() //<div id="svoi" class="zONas-
+          //N = $() //Путь до содержания окна
+        
+          setTimeout(() => {
+            zONas.L(d, b, N, 'Тут ваш текст! <b>оп оп!</b>');
+          }, 3000);
+        }
+      });
+      
+  */
+  $: (q={}) => {//Вешаем click
+    let O = zONas, on;
+    
     O.oko(m => { //Отслеживаем изменения размера браузер окна
       let b,
         w = m.contentRect.width,
         h = m.contentRect.height;
-    
+
       //Браузер oкно != Последняя запись проверки
       if (w != O.R || h != O.R) { //Изменили размер Браузер окна width
         b = $('.zONasO, .zONasOm');
-          
-        if(b[0]) {//Закроем
-          //console.error('Закроем'); 
+
+        if (b[0]) { //Закроем
+          //console.error('O.oko() Закроем/Откроем'); 
           b.find('>button:eq(0),:eq(0) button')
-          .click()//Закроем
-          .click()//Откроем (Для точного отображения)
+            .click() //Закроем
+            .click() //Откроем (Для точного отображения)
         }
 
         O.R = [w, h]; //Обновим
       }
     });
+      
+    on = id => {//Вешаем click
+      //console.debug('each', id);
+        
+      id.each((i, e) => {
+        if ($._data($(e)[0], 'events')?.click[0].namespace != 'zONas') { //Проверка ключа `Ключь не совпал! повесим обработчик`
+          $(e).on('click.zONas', e => {
+            //console.debug('click');
+            O.C($(e.currentTarget), q?.F);
+          });
+        } else {
+          console.debug('Была попытка повторного запуска скрипта zONas.$();', e);
+        }
+       });
+    };
+    
+    if(q.id && (q.id = $('body #' + q.id))[0]) {//По id="svoi"
+      on(q.id);
+    } else {//Поиск всех
+      on($('[class*="zONas-"]').not('[id]').find('>button:eq(0),>:eq(0) button'))
+    }
   },
-  /* zONas.C(//Обработка. Запуск: zONas.oko()//Отслеживаем изменения размера браузер окна
+  /* zONas.C(//Обработка
        $(),//id окна
-       1   //Запускаем для проверки
+       F   //Для запуска function при открытии окна. zONas.$(q);
+           //F=0 //Для отмены запуска Функции
      );
   */
-  C: (b, x) => {//Обработка click 'b = button'
-    let d = b.closest('[class*="zONas-"]'),//Вокруг кнопки
-      N=d.find('>div').eq(-1);//Содержание
+  C: (b, F) => {//Обработка click 'b = button'
+    zONas.F = F? 1 : 0;//Ожидается подгрузка содержания (zONas.$({F: () => {}});)
     
-    if(!x && /zONasOm?/.test(d.attr('class'))){//Окно открыто `Закрываем`
+    let d = b.closest('[class*="zONas-"]'),//Вокруг кнопки
+      N = d.find('>div').eq(-1);//Содержание
+      
+    if (F) {
+      N.text('Загрузка...')
+    }
+    
+    if(/zONasOm?/.test(d.attr('class'))){//Окно открыто `Закрываем`
       d.removeClass('zONasO zONasOm');
       N.removeAttr('style');
-    } else {//Открываем/Проверка x=1
+    } else {//Открываем
+      if (F) {
+        F(d, b, N)
+      }
       let D = $(document),//Полный размер документа С прокруткой (Применять до display:'unset')
         w = D.outerWidth(),
         h = D.outerHeight();
         
       //console.debug(N.find('>div').html().replace(/[\r\n\t ]/g, '').length+' <= Количество символов содержания');
-      if(N.find('>div').html().replace(/[\r\n\t ]/g, '').length==0) {
+      if((N.find('>div').html() || '').replace(/[\r\n\t ]/g, '').length==0) {
         N.find('>div').html('<font color="red">Содержание отсутствует!</font>')
       }
       
-      if(!x) {//x=1; Запускаем для проверки
-        setTimeout(() => {
-          $(document).on('click.zONas', e => {//• Клик вне элемента  $()
-             //если клик был не по нашему блоку && и не по его дочерним элементам
-              if (!N.is(e.target) && !N.has(e.target)[0]) {//Клик вне элемента
-                $(document).off('.zONas');//† Удалим click
-                d.removeClass('zONasO zONasOm');//† Закрываем
-                N.removeAttr('style');
-              }
-          });
-        }, 1);//Убераем срабатывание click при открытии
+      setTimeout(() => {
+        $(document).on('click.zONas', e => {//• Клик вне элемента  $()
+          //если клик был не по нашему блоку && и не по его дочерним элементам
+          if (!N.is(e.target) && !N.has(e.target)[0]) {//Клик вне элемента
+            $(document).off('.zONas');//† Удалим click
+            d.removeClass('zONasO zONasOm');//† Закрываем
+            N.removeAttr('style');
+          }
+        });
+      }, 1);//Убераем срабатывание click при открытии
       
-        N.css({display:'unset'});//Для определения: .offset()
-      }
+      N.css({display:'unset'});//Для определения: .offset()
+
       B = zONas.w(d, N, w, h);//сменим направление
 
       d.addClass(
@@ -79,20 +120,20 @@ var zONas = {//Всплывающее окно `Настройки/Разное`
       );
     }
   },
+  /* d = $() //<div id="svoi" class="zONas-
+     b = $() //<button
+     N = $() //Путь до содержания окна */
+  L: (d, b, N, t) => {//Подгрузка содержимого
+    if(zONas.F) {
+      d.click();//Закроем
+      N.html(t);//Подгрузили текст
+      zONas.C(b)
+    }
+  },
   //d = Вокруг кнопки`zONas-[A-Z]`, n = Содержание окна, D = Полный размер документа С прокруткой
   w: (d, n, w, h) => {//Когда не помещяется на экране`горизонтально`, сменим направление
     let B, O = 10,//Отступ от стенок
-      //b = $(window),
-      //Bw = b.innerWidth(),//Размер браузер окна
-      //Bh = b.innerHeight(),
-      //sL = Math.round(b.scrollLeft()),//Отступ прокрутки
-      //sT = Math.round(b.scrollTop()),
       o = n.offset(),//Координаты left|top относительно окна + прокрутка
-      //Ls = Math.round(sL + o.left),//Прокрутка + До окна
-      //Ts = Math.round(sT + o.top),
-      
-      //L= Math.round(o.left - b.scrollLeft()),//Координаты left относительно окна + прокрутка
-      //T= Math.round(o.top - b.scrollTop()),  //top До окна без прокрутки (Видимая часть)
       
       W = Math.round(n.outerWidth() + O),//Размер всплыв окна + отступ
       H = Math.round(n.outerHeight() + O),
@@ -103,50 +144,45 @@ var zONas = {//Всплывающее окно `Настройки/Разное`
       Lx = o.left < O || LW > w,//Окно не помещяется горизонт
       Tx = o.top < O || TH > h;//вертикаль
     
-    
-    //console.debug('Bw: ' + Bw + ', Bh: ' + Bh+' <= Браузер окно');
-    console.debug('w: ' + w + ', h: ' + h+' <= Размер документа с прокруткой');
+    /*console.debug('w: ' + w + ', h: ' + h+' <= Размер документа с прокруткой');
     console.debug('W: '+W+', H: '+H+' <= Размер окна');
     console.debug('o.left: ' + Math.round(o.left)+', o.top: ' + Math.round(o.top)+' <= Координаты до окна');
     console.debug('LW: '+LW+', TH: '+TH+' <= До окна + окно');
-    //console.debug('sL: ' + sL + ', sT: ' + sT+' <= scroll Прокрутка');
-    //console.debug('L: ' + L + ', T: ' + T+' <= scroll + o.left');
-    //console.debug('Ls: ' + Ls + ', Ts: ' + Ts +' <= Прокрутка + До окна');
     console.debug(
       'Tx: top['+Math.round(o.top)+'] < ['+O+']O || TH['+TH+'] > ['+h+']h (' + (Tx ? 'Не ' : '') + 'помещается top) <= До окна + окно\n' +
       'Lx: left['+Math.round(o.left)+'] < ['+O+']O || LW['+LW+'] > ['+w+']w (' + (Lx ? 'Не ' : '') + 'помещается left)'
-    );
+    );*/
     
     if (Lx || Tx) {//Не помещается
-      console.log('-');
+      //console.log('-');
       let S, C = 'zONas-';
       
       d.removeClass((i, c) => {//Сработает 1 раз 
         S = /zONas-[A-Z]+/.exec(c)[0];//Нашли zONas-..
         //S[6];//T = Верх   || B = Низ
         //S[7];//L = В лево || R = В право
-        console.debug('Удалили class: '+S);
+        //console.debug('Удалили class: '+S);
         return S//Удаляем
       });
       
-      console.debug('if(o.top['+Math.round(o.top)+'] < ['+O+']O || TH['+TH+'] > ['+h+']h) => '+Tx);
+      //console.debug('if(o.top['+Math.round(o.top)+'] < ['+O+']O || TH['+TH+'] > ['+h+']h) => '+Tx);
       if (Tx) {//Не помещается вертикал. Меняем сторону
         C += S[6] == 'B'? 'T':'B';
-        console.debug((S[6] == 'B'? 'T':'B')+' <= Меняем сторону. '+C);
+        //console.debug((S[6] == 'B'? 'T':'B')+' <= Меняем сторону. '+C);
       } else {
         C+=S[6]
       }
       
       //console.debug('if(o.left['+Math.round(o.left)+'] < ['+O+']O || (до окна + окно)Lh['+Lh+'] > ['+w+']w) => '+Lx);
       if(Lx) {//Не помещается горизонт
-        console.debug('if((S[7]'+S[7]+'==`L` && (o.left['+Math.round(o.left)+'] < ['+O+']O && LW['+LW+'] > ['+w+']w)) || (S[7]'+S[7]+'==`R` && o.left['+Math.round(o.left)+'] > ['+W+']W)) => '+ ((S[7]=='L' && (o.left < O && LW > w)) || (S[7]=='R' && o.left > W)));
+        //console.debug('if((S[7]'+S[7]+'==`L` && (o.left['+Math.round(o.left)+'] < ['+O+']O && LW['+LW+'] > ['+w+']w)) || (S[7]'+S[7]+'==`R` && o.left['+Math.round(o.left)+'] > ['+W+']W)) => '+ ((S[7]=='L' && (o.left < O && LW > w)) || (S[7]=='R' && o.left > W)));
         //          с права есть место || с лева есть место <= (Вышло за пределы)
         if((S[7]=='L' && (o.left < O && LW > w)) || (S[7]=='R' && o.left > W)){//Меняем сторону
           C += S[7] == 'R'? 'L':'R';
-          console.debug((S[7] == 'R'? 'L':'R')+' <= Меняем сторону. '+C);
+          //console.debug((S[7] == 'R'? 'L':'R')+' <= Меняем сторону. '+C);
         } else {//Не можем сменить сторону `Не хватает места`
           C += S[7];//Оставим сторону и сместим окно
-          console.debug((S[7]=='R'?'left':'right')+': '+(S[7] == 'R' ?'w['+w+'] - ['+LW+']LW - ['+O+']O = '+(w - LW - O):'o.left['+o.left+']')+'px <= Сместим окно. '+C);
+          //console.debug((S[7]=='R'?'left':'right')+': '+(S[7] == 'R' ?'w['+w+'] - ['+LW+']LW - ['+O+']O = '+(w - LW - O):'o.left['+o.left+']')+'px <= Сместим окно. '+C);
           
           n.css({[S[7]=='R'?'left':'right']:
             S[7]=='R'
